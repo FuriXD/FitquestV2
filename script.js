7627692698f6a32ccf70f193b53ff4bf178c6cf3
Mohd Ariz <90212547+FuriXD@users.noreply.github.com>
2024-12-07 10:31:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Add event listeners to menu icons
+document.getElementById('home-icon').addEventListener('click', () => {
+    alert('Navigating to Home!');
+  });
+  
+  document.getElementById('stats-icon').addEventListener('click', () => {
+    alert('Opening Stats!');
+  });
+  
+  document.getElementById('add-icon').addEventListener('click', () => {
+    alert('Add new activity!');
+  });
+  
+  document.getElementById('health-icon').addEventListener('click', () => {
+    alert('Viewing Health Info!');
+  });
+  
+  document.getElementById('profile-icon').addEventListener('click', () => {
+    alert('Opening Profile!');
+  });
+  
